7969f9652dafe2a7bc44df66a51f0373be2bb63a
kajal <you@example.com>
2020-02-02 12:43:20 +0530

    updated

--- routes/product.js
+++ routes/product.js
@@ -46,4 +46,65 @@ app.get("/getapi/:id",(req,res)=>{
     })
 })
 
+
+//  thid code
+
+app.get("/apidata/:cart_id",(req,res)=>{
+    let cart_id = req.params.cart_id
+    let datastore = appDB.cart_get_by_id(cart_id)
+    datastore.then((resp_data)=>{
+        let not_india_tax = 10
+        let indian_tax = 5
+        let imported = resp_data[0]["imported"]
+        let category = resp_data[0]["category"]
+        let price = resp_data[0]["price"]
+        if (imported == "not_indian"){
+            var tax_data = price * not_india_tax/100
+            var price_tax = price + tax_data
+            let data_dic = {
+                "cart_id" : resp_data[0]["cart_id"],
+                "quantity" : resp_data[0]["quantity"],
+                "product_name" : resp_data[0]["product_name"],
+                "imported" : resp_data[0]["imported"],
+                "category" : resp_data[0]["category"],
+                "price" : resp_data[0]["price"],
+                "quantity_prise" : resp_data[0]["quantity_prise"],
+                "price_tax" : tax_data,
+                "price_with_tax " : price_tax
+            }
+            res.send(data_dic)
+            }
+        else if(imported == "indian"){
+            if (category == "general"){
+                var tax_amount = price * indian_tax/100
+                var price_with_tax = price + tax_amount
+            let data_dic = {
+                "cart_id" : resp_data[0]["cart_id"],
+                "quantity" : resp_data[0]["quantity"],
+                "product_name" : resp_data[0]["product_name"],
+                "imported" : resp_data[0]["imported"],
+                "category" : resp_data[0]["category"],
+                "price" : resp_data[0]["price"],
+                "quantity_prise" : resp_data[0]["quantity_prise"],
+                "tax_data" : tax_amount,
+                "price_with_tax_ind" : price_with_tax
+            }
+            res.send(data_dic)
+        }
+        else{
+            let data_dic = {
+                "cart_id" : resp_data[0]["cart_id"],
+                "quantity" : resp_data[0]["quantity"],
+                "product_name" : resp_data[0]["product_name"],
+                "imported" : resp_data[0]["imported"],
+                "category" : resp_data[0]["category"],
+                "price" : resp_data[0]["price"],
+                "quantity_prise" : resp_data[0]["quantity_prise"]
+            }
+            res.send(data_dic)
+        } 
+       }
+   
+    })
+});
 module.exports = app;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ app.use(bodyParser.json())
 
 const sales_tax = require("./routes/product")
 app.use("/apis",sales_tax)
+ 
 
 app.listen(5000,()=>{
     console.log("server is listening..............)")
