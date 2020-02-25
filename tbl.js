e7e7db14aab29d663a0b3a157d7c832f8bb1e1dd
kajal kumari <40683741+Kajal2000@users.noreply.github.com>
2020-02-25 12:48:08 +0530

    updated tbl file

--- tbl.js
+++ tbl.js
@@ -1,6 +1,6 @@
 let knex = require('./connection.js')
 
-// first table !!!!! product tbl
+// first product tbl
 
 knex.schema.createTable('products_tbl', (table) => {
     table.string('product_name');
@@ -18,16 +18,16 @@ knex.schema.createTable('products_tbl', (table) => {
 // second table !!!!! cart tbl
 
 
-// knex.schema.createTable('cart', (table) => {
-//     table.increments('cart_id')
-//     table.string('product_name');
-//     table.string('imported')
-//     table.string('category');
-//     table.float('price')
-//     table.integer('quantity')
-//     table.integer('quantity_prise')
-//     }).then(()=>{
-//         console.log("table has created")
-//     }).catch((err) => {
-//         console.log(err,"There is some err while writing the query")
-//     })
+knex.schema.createTable('cart', (table) => {
+    table.increments('cart_id')
+    table.string('product_name');
+    table.string('imported')
+    table.string('category');
+    table.float('price')
+    table.integer('quantity')
+    table.integer('quantity_prise')
+    }).then(()=>{
+        console.log("table has created")
+    }).catch((err) => {
+        console.log(err,"There is some err while writing the query")
+    })
